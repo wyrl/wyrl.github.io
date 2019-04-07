931f9e1690cb87d84808bd28eda35eb5a8d1bc58
Wyr'l D. Villazorda <wyrlvillazorda@gmail.com>
2019-04-07 16:46:39 +0800

    added files for calculator demo

---
+++ my_demos/reactjs/calculator/precache-manifest.91a67f7e286d54c6a5920642f412a7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cede1e92dfdf1a482d73",
+    "url": "/my_demos/reactjs/calculator/static/js/runtime~main.cede1e92.js"
+  },
+  {
+    "revision": "ff6eeb22c2d1ba3edff1",
+    "url": "/my_demos/reactjs/calculator/static/js/main.ff6eeb22.chunk.js"
+  },
+  {
+    "revision": "25cdb81fc960b76de106",
+    "url": "/my_demos/reactjs/calculator/static/js/2.25cdb81f.chunk.js"
+  },
+  {
+    "revision": "ff6eeb22c2d1ba3edff1",
+    "url": "/my_demos/reactjs/calculator/static/css/main.d98afefb.chunk.css"
+  },
+  {
+    "revision": "25cdb81fc960b76de106",
+    "url": "/my_demos/reactjs/calculator/static/css/2.ba4ac2fa.chunk.css"
+  },
+  {
+    "revision": "a3e86b32f38835d3d96598900c643d07",
+    "url": "/my_demos/reactjs/calculator/index.html"
+  }
+];
